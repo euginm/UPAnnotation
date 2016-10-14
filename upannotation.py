996b7572b7bbf66b5e78820955aafc2e8123e688
euginm <zhenich13@gmail.com>
2016-10-14 14:47:30 +0200

    Add files via upload

--- upannotation.py
+++ upannotation.py
@@ -18,15 +18,18 @@ def get_full_info(query, f, t='ACC', form='txt'):
 
     data = urllib.urlencode(params)
     request = urllib2.Request(url, data)
+
     try:
         response = urllib2.urlopen(request)
         page = response.read()
+        response.close()
         return page
     # try twice before give up
     except urllib2.HTTPError:
         try:
             response = urllib2.urlopen(request)
             page = response.read()
+            response.close()
             return page
         except urllib2.HTTPError as e:
             print e.code
@@ -120,12 +123,18 @@ def extend_table(**kwargs):
     # obtain the key and alt_key column number
     header = table.next()
 
+    categories = "ACC+ID ACC ID UPARC NF50 NF90 NF100 GENENAME EMBL_ID EMBL P_ENTREZGENEID P_GI PIR REFSEQ_NT_ID P_REFSEQ_AC UNIGENE_ID PDB_ID DISPROT_ID BIOGRID_ID DIP_ID MINT_ID STRING_ID CHEMBL_ID DRUGBANK_ID GUIDETOPHARMACOLOGY_ID SWISSLIPIDS_ID ALLERGOME_ID ESTHER_ID MEROPS_ID MYCOCLAP_ID PEROXIBASE_ID REBASE_ID TCDB_ID BIOMUTA_ID DMDM_ID WORLD_2DPAGE_ID DNASU_ID ENSEMBL_ID ENSEMBL_PRO_ID ENSEMBL_TRS_ID ENSEMBLGENOME_ID ENSEMBLGENOME_PRO_ID ENSEMBLGENOME_TRS_ID GENEDB_ID P_ENTREZGENEID KEGG_ID PATRIC_ID UCSC_ID VECTORBASE_ID WBPARASITE_ID ARACHNOSERVER_ID CCDS_ID CGD CONOSERVER_ID DICTYBASE_ID ECHOBASE_ID ECOGENE_ID EUHCVDB_ID EUPATHDB_ID FLYBASE_ID GENECARDS_ID GENEREVIEWS_ID H_INVDB_ID HGNC_ID HPA_ID LEGIOLIST_ID LEPROMA_ID MAIZEGDB_ID MGI_ID MIM_ID NEXTPROT_ID ORPHANET_ID PHARMGKB_ID POMBASE_ID PSEUDOCAP_ID RGD_ID SGD_ID TAIR_ID TUBERCULIST_ID WORMBASE_ID WORMBASE_PRO_ID WORMBASE_TRS_ID XENBASE_ID ZFIN_ID EGGNOG_ID GENETREE_ID HOGENOM_ID HOVERGEN_ID KO_ID OMA_ID ORTHODB_ID TREEFAM_ID BIOCYC_ID REACTOME_ID UNIPATHWAY_ID CLEANEX_ID COLLECTF_ID CHITARS_ID GENEWIKI_ID GENOMERNAI_ID"
     key = kwargs.get("key")
     category = kwargs.get("category")
+    if category not in categories.split():
+        sys.exit("\033[91m" + category + " is not a correct category name." + "\033[0m" + "\n" +
+                 "Category names can be found here:" + "\n"
+                 "http://www.uniprot.org/help/programmatic_access#id_mapping_examples")
+
     try:
         header.index(key)
     except ValueError:
-        sys.exit("Can't find column named " + key + " in input table")
+        sys.exit("\033[91m" + "Can't find column named " + key + " in input table" + "\033[0m")
 
     key_index = header.index(key)
 
@@ -133,12 +142,16 @@ def extend_table(**kwargs):
     alt_category = kwargs.get("alt_category")
     if alt_key:
         if not alt_category:
-            sys.exit('Please provide alternative category')
+            sys.exit("\033[91m" + "Please provide alternative category" + "\033[0m")
+        if alt_category not in categories.split():
+            sys.exit("\033[91m" + alt_category + " is not a correct category name." + "\033[0m" + "\n" +
+                     "Category names can be found here:" + "\n" +
+                     "http://www.uniprot.org/help/programmatic_access#id_mapping_examples")
         try:
             header.index(alt_key)
             alt_index = header.index(alt_key)
         except ValueError:
-            print "Can't find column named " + alt_key + " in input table"
+            print "\033[91m" + "Can't find column named " + alt_key + " in input table" + "\033[0m"
 
     # write new header
     columns = kwargs.get("columns").split(',')
@@ -159,11 +172,12 @@ def extend_table(**kwargs):
             header.append(col_names[col])
             order.append(col)
         except KeyError:
-            sys.exit("Possible column names are ID, LEN, AC, DE, CC, GO, LINK")
+            sys.exit("\033[91m" + "Possible column names are ID, LEN, AC, DE, CC, GO, LINK" + "\033[0m")
 
     output_table.writerow(header)
 
     # write the output table
+    processed = 0
     for row in table:
         mapping_succeed = True
         full_info = get_full_info(row[key_index], f=category)  # mapping function
@@ -188,6 +202,10 @@ def extend_table(**kwargs):
             else:
                 error_mesage = "ERROR: unable to map " + row[key_index] + " to any protein in UniProt DB"
             output_table.writerow(row + [error_mesage])
+
+        processed += 1
+        if processed % 25 == 0:
+            print "\033[94m" + str(processed) + " rows processed" + "\033[0m"
 
 
 if __name__ == '__main__':
